b6a5547094140e9a420c19ed0c5fe6728664857f
giosue_cattelan <giosuecattelan10@gmail.com>
2024-05-27 21:24:07 +0200

    removed useless pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,34 @@
 <template>
-  <div id="diomerda" class="pattern">
-    <p class="text-h1 font-nippo text-palette-secondary">Giosuè Cattelan🫡</p>
+  <div class="bg-palette-primary grid grid-cols-12 h-svh">
+    <div class="sideNav bg-palette-600 col-start-1 row-span-1">
+      <nav class="left-nav">
+        <ul class="flex gap-4">
+          <li>
+            <NuxtLink to="/"
+              ><span class="material-symbols-outlined"> home </span></NuxtLink
+            >
+          </li>
+          <li><NuxtLink to="/about">O</NuxtLink></li>
+          <li><NuxtLink to="/products">n</NuxtLink></li>
+        </ul>
+      </nav>
+    </div>
+    <p class="col-start-2 col-span-11 text-h1 text-palette-accent_2">
+      Giosuè Cattelan🫡
+    </p>
   </div>
 </template>
 
 <style scoped>
-p {
-  @apply mb-3;
-}
 .pattern {
   width: 100%;
+  min-height: 100vh;
   height: 100%;
-  /* Add your background pattern here */
-  background-color: #313131;
+  /* background-color: theme("colors.palette.primary");
   background-image: radial-gradient(
-    rgba(255, 255, 255, 0.171) 2px,
+    theme("colors.palette.accent_2") 2px,
     transparent 0
-  );
+  ); */
   background-size: 30px 30px;
   background-position: -5px -5px;
 }
